c347c716e699ca1f0ee3898a4b6c7d2936a52aba
Kalamath <thecodedimage@gmail.com>
2019-07-26 16:48:42 -0700

    background image

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
-$(function() {
-    $('#datetimepicker3').datetimepicker({
-      pickDate: false
-    });
-  });
+// Firebase JS
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDjA2Lp4Qga0i6vs4xUH4-1tCm-nwOYgIk",
+    authDomain: "fullstackclass-15e91.firebaseapp.com",
+    databaseURL: "https://fullstackclass-15e91.firebaseio.com",
+    projectId: "fullstackclass-15e91",
+    storageBucket: "",
+    messagingSenderId: "568587410686",
+    appId: "1:568587410686:web:38a6ac40b6d1a074"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  
